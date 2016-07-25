b5318efc05d2bfb05c24949201ca6a345d71ce4c
https://github.com/uwwdsog/mym.git <wsteele@salesforce.com>
2016-07-25 16:44:02 -0400

    update campaign member config

--- makefile.sh
+++ makefile.sh
@@ -3,9 +3,4 @@ git commit -m "update campaign member config"
 git push heroku master
 #
 #git push heroku master --force
-<<<<<<< HEAD
 heroku open --app campaignmemberdetails
-=======
-# heroku open --app campaignmemberdetails
->>>>>>> beb94a01401d55fddf6477a84c9b33f047609055
-
