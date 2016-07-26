a4380a6e21460fbff4f2d6a783736acd533cf500
https://github.com/uwwdsog/mym.git <wsteele@salesforce.com>
2016-07-26 18:48:44 -0400

    update campaign member config

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post('/campaignmemberdetails', function (request, response) {
 	var validationErrors  = "";
 	
 	validationErrors=performValidations(newCampaignDetail);
-	console.log("Validation errors: "+validationErrors);
+	//console.log("Validation errors: "+validationErrors);
 	if  (validationErrors == null || validationErrors.length<=1) {
 		console.log("Validation errors: "+validationErrors);
 		response.send("Validation errors: "+validationErrors);
