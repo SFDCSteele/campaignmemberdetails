35c2287c17d191696d974efbd4d5e28de10a4524
https://github.com/uwwdsog/mym.git <wsteele@salesforce.com>
2016-07-26 12:58:49 -0400

    update campaign member config

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ var pg = require('pg');
 
 var main_sql = "";
 pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -21,20 +31,19 @@ function handleError(res, reason, message, code) {
 
 app.get('/', function(request, response) {
   response.render('pages/index');
-});
+}
 
-//SELECT * FROM test_table
-//select * from campaign_member_activity__c
 app.get('/campaignmemberdetails', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  //pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     //client.query('select * from uwwsharedcrm.campaign_member_activity__c', function(err, result) {
-    client.query(executeQuery(1), function(err, result) {
+    pg.client.query(buildQuery(1), function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
        { 
        	console.log ("1-rows: "+result.rows);
+		console.log(JSON.stringify(result.rows));
        	displayObject(result.rows);
        	response.render('pages/campaignmemberdetails', {results: result.rows} 
        ); }
@@ -42,11 +51,56 @@ app.get('/campaignmemberdetails', function (request, response) {
   });
 })
 
+
+app.post('/campaignmemberdetails', function (request, response) {
+	var newCampaignDetail = request.body;
+	var validationErrors  = "";
+	
+	if  ((validationErrors=performValidations(newCampaignDetail)) != null) {
+		response.send("Validation errors: "+validationErrors);
+	}
+	
+	if (!campaignExists(newCampaignDetail)) {
+		response.send("Non-existent campaign ID");
+	}
+	
+	
+	var sqlInsert = "insert into campaign_details (";
+	var sqlFields = "";
+	var sqlValues = ") values (";
+	var i = 0;
+	
+	for (var prop in newCampaignDetail) {
+	    if (newCampaignDetail.hasOwnProperty(prop)) {
+	    	if ( i++ > 0 ) {
+	    		sqlFields += ",";
+	    		sqlValues += ",";
+	    	}
+	    	sqlFields += prop;
+	    	sqlValues += "'" + newCampaignDetail[prop] + "'";
+	        console.log(prop +"-->"+newCampaignDetail[prop]);
+	    }
+	}    
+    //pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    pg.client.query(sqlInsert+sqlFields+sqlValues+")", function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err+"<table><tr><td>datebase   url</td><td>"+process.env.DATABASE_URL+"</td></tr>"+
+                                                          "<tr><td>sql statement:</td><td>"+sqlInsert+sqlFields+sqlValues+")"+"</td></tr></table>"); }
+      else
+       { 
+	    response.send(200);
+     	//response.render('pages/db', {results: result.rows} );
+       }
+    //});
+  });
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-function executeQuery (opt) {
+function buildQuery (opt) {
 	var rtnSQL = "";
 	if (opt == 1 ) {
 		rtnSQL = "select "+
@@ -70,10 +124,38 @@ function executeQuery (opt) {
 			"a.Future_1__c as \"Future Result\" "+
 			"from uwwsharedcrm.campaign_member_activity__c a, uwwsharedcrm.campaign c "+
 			"where a.Campaign__c=c.sfid";
+	} else if ( opt == 2 ) { //lookup campaign
+		rtnSQL = "select sfid from uwwsharedcrm.campaign where sfid=\"";
 	}
 	console.log("returning SQL: "+rtnSQL);
 	return rtnSQL;
 	
+}
+
+function performValidations(body) {
+	var rtnErrors;
+	if (!(body.FirstName || body.LastName)) {
+		rtnErrors+= "Invalid user input", "Must provide a first and last name.";
+	}
+	if (!(body.email) {
+		rtnErrors+= "Invalid user input", "Must provide an email address.";
+	}
+	if (!(body.PostalCode) {
+		rtnErrors+= "Invalid user input", "Must provide a postal code.";
+	}
+	return rtnErrors;
+}
+
+function campaignExists (body) {
+    pg.client.query(buildQuery(2)+body.Campaign__c+"\"", function(err, result) {
+      //done();
+		if (err) {
+      		console.error(err); 
+      		return false;
+      	} else { 
+			return true;
+		}
+    });	
 }
 
 function displayObject(rows) {
