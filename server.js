54695876abb2ae2f356a77686892207f7bc82c56
https://github.com/uwwdsog/mym.git <wsteele@salesforce.com>
2016-07-28 13:58:58 -0400

    update campaign member config

--- server.js
+++ server.js
@@ -48,7 +48,12 @@ app.use(cors());
 app.options('*',cors(),function(){
 console.log("preflight")
 });
-
+module.exports = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');  
+  next();
+};
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -80,7 +85,7 @@ app.get('/campaignmemberdetails', function (request, response, next) {
   });
 });
 
-app.post('/campaignmemberdetails', function (request, response, next) {
+app.post('/campaignmemberdetails', cors, function (request, response) {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   //next();
